9b316a0f2403cdb9c44f3a21440e42847c8926b8
Yiop <mm.bustos@alumno.etec.um.edu.ar>
2026-09-14 10:04:17 -0300

    no funciona el tex2num xd

---
+++ Matematicas/Numeros_Nombre.py
@@ -0,0 +1,10 @@
+from text2num import text2num
+
+texto = "tres mil doscientos cuarenta y cinco"
+
+# Convertimos el texto a su número correspondiente en español ('es')
+numero = text2num(texto, lang="es")
+
+print(f"Resultado numérico: {numero}")
+# Salida: Resultado numérico: 3245
+
